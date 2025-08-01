463ae8c4d913f333af6dd7effbd3108979d982c4
oswaldo <oswaldoleon72@gmail.com>
2025-08-01 01:33:05 -0400

    limpieza

--- skyguard.apps.gps.services.email_processor.py
+++
@@ -1,6 +0,0 @@
-            # Create new device
-            device = GPSDevice.objects.create(
-                imei=imei,
-                name=f"AUTO_{imei:015d}",
-                comments=f"Auto-registered from email: {imei_data}"
-            ) 
